3b711ec901f145aac023b9c900a1375e0f887bef
Adry020304 <najerafranklin2003@gmail.com>
2024-10-18 19:59:29 -0400

    Initial Commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Adrian Najera on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
